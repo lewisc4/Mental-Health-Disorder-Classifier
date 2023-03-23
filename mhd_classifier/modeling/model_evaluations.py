d3375fa5b042f52f04b48690ae41aa983f40e1f7
Chris Lewis <51756603+lewisc4@users.noreply.github.com>
2023-03-23 19:50:03 -0400

    Updated model_evaluations.py
    Added a metric name-function map to easily call a metric's function using its corresponding name. Also added default column names for prediction and true label ID columns and the ability to provide a title prefix for figures to differentiate them better.

--- mhd_classifier/modeling/model_evaluations.py
+++ mhd_classifier/modeling/model_evaluations.py
@@ -1,4 +1,6 @@
 import json
+import sklearn
+import matplotlib
 import matplotlib.pyplot as plt
 
 from sklearn.manifold import TSNE
@@ -6,38 +8,65 @@ from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 from sklearn.metrics import precision_recall_fscore_support as score
 
 
-class ModelEvaluator:
+# Score names to use/return from  the precision_recall_fscore_support function
+SCORE_NAMES = ['Precision', 'Recall', 'F1_Score']
 
-	def __init__(self, predictions, embeddings, pred_col, true_col, label_id_map):
+
+class ModelEvaluator:
+	''' Class to calculate evaluation metrics using model predictions. '''
+
+	def __init__(
+		self,
+		predictions,
+		embeddings,
+		label_id_map,
+		pred_col='predicted',
+		true_col='actual',
+		title_prefix=''
+	):
+		# Model prediction DF and prediction embeddings
 		self.predictions = predictions
 		self.embeddings = embeddings
-		
+		# Dict to map label IDs (keys) to their corresponding label name
+		self.label_id_map = label_id_map
+		# DF columns for predicted and true (i.e. actual) label IDs
 		self.pred_col = pred_col
 		self.true_col = true_col
-		
+		# Prefix to use in figure titles (useful for differentiating plots)
+		self.title_prefix = title_prefix.strip() + ' '
+		# Predicted/true (i.e. actual) label IDs
 		self.y_pred = self.predictions[self.pred_col]
 		self.y_true = self.predictions[self.true_col]
-		
-		self.label_id_map = label_id_map
-
+		# Unique label names and all predicted/true (i.e. actual) label names
 		self.labels = list(self.label_id_map.values())
 		self.pred_labels = self.y_pred.map(self.label_id_map)
 		self.true_labels = self.y_true.map(self.label_id_map)
+		# Maps a metric name to its corresponding function
+		self.metric_function_map = {
+			'scores': self.get_scores,
+			'confusion_matrix': self.get_confusion_matrix,
+			'embedding_visualization': self.get_pred_visuals,
+		}
 
 
-	def get_metrics(self):
-		metric_keys = ['Precision', 'Recall', 'F1_Score']
-		metrics = score(self.true_labels, self.pred_labels, labels=self.labels)
-
-		label_metrics = [dict(zip(self.labels, metric)) for metric in metrics]
-		grouped_metrics = dict(zip(metric_keys, label_metrics))
-		return json.dumps(grouped_metrics, sort_keys=True, indent=4)
+	def get_metric(self, metric_name):
+		''' Gets a metric based on its name. '''
+		return self.metric_function_map.get(metric_name, lambda: None)()
 
 
-	def get_confusion_matrix(self, title='Confusion Matrix'):
+	def get_scores(self):
+		''' Get per-class precision, recall, and F1-scores from predictions. '''
+		scores = score(self.true_labels, self.pred_labels, labels=self.labels)
+
+		label_scores = [dict(zip(self.labels, metric)) for metric in scores]
+		grouped_scores = dict(zip(SCORE_NAMES, label_scores))
+		return json.dumps(grouped_scores, sort_keys=True, indent=4)
+
+
+	def get_confusion_matrix(self):
+		''' Gets a confusion matrix (matplotlib figure) from predictions. '''
 		fig, ax = plt.subplots(figsize=(10, 10))
-		plt.title(title)
-
+		plt.title(self.title_prefix + 'Confusion Matrix')
 		conf_mat = confusion_matrix(
 			self.true_labels,
 			self.pred_labels,
@@ -49,15 +78,17 @@ class ModelEvaluator:
 			display_labels=self.labels
 		)
 		disp.plot(ax=ax, cmap=plt.cm.Blues)
-		return plt
+		# Return the current pyplot Figure
+		return plt.gcf()
 
 
-	def get_pred_visuals(self, n_components=2, alpha=0.7, title='Pred Visuals'):
+	def get_pred_visuals(self, n_components=2, alpha=0.7):
+		''' Gets node embedding visualizations (matplotlib figure) from embeddings. '''
 		# Project/transform the embeddings into 2-Dimensional space
 		transform = TSNE
 		trans = transform(n_components=n_components)
 		embeddings_2d = trans.fit_transform(self.embeddings)
-
+		
 		# Plot 2-D embeddings for each class
 		plt.figure(figsize=(7, 7))
 		plt.axes().set(aspect='equal')
@@ -69,11 +100,11 @@ class ModelEvaluator:
 			alpha=alpha,
 		)
 		handles, plt_labels = scatter.legend_elements()
-		# plt_labels has '$\\mathdefault{0}$', '$\\mathdefault{1}$', etc. instead of ints
 		label_ids = [int(''.join(i for i in x if i.isdigit())) for x in plt_labels]
 		embedding_labels = [self.label_id_map[lbl_id] for lbl_id in label_ids]
 
 		plt.legend(handles=handles, labels=embedding_labels)
-		plt.title(title)
-		return plt
+		plt.title(self.title_prefix + 'Node Embedding Visualizations')
+		# Return the current pyplot Figure
+		return plt.gcf()
 
