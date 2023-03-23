ff224c55715bf6a463ffbf4960c89c60f74e101c
Chris Lewis <51756603+lewisc4@users.noreply.github.com>
2023-03-23 19:38:53 -0400

    Updated cli_utils.py
    Updated cli_utils.py by splitting parsing into separate functions, one for each CLI script (preprocess.py, train.py, evaluate.py). All functions share the same parent parser, to minimize unnecessary overlap.

--- mhd_classifier/utils/cli_utils.py
+++ mhd_classifier/utils/cli_utils.py
@@ -1,35 +1,43 @@
 import argparse
+import sklearn
 
 from pathlib import Path
 
 
+# The CWD of the script importing this module
 CWD = Path().resolve()
-DATASET_DIR = Path(CWD, 'data/dataset')
-PATTERN_DIR = Path(CWD, 'data/keywords/patterns')
-SYNONYM_DIR = Path(CWD, 'data/keywords/synonyms')
-MATCH_DIR = Path(CWD, 'data/keywords/matches')
-PROCESSED_DIR = Path(CWD, 'data/processed')
-MODEL_DIR = Path(CWD, 'data/models')
-EVALUATION_RESULTS_DIR = Path(CWD, 'data/evaluation_results')
-AVAILABLE_DISORDERS = ['anxiety', 'depression', 'adhd', 'ocd', 'eating_disorders']
+
+# The "root" data dir, containing all project resource files (by default)
+DATA_DIR = CWD / 'data'
+# Where the initial dataset (nodes/edges) is stored
+DATASET_DIR = DATA_DIR / 'dataset'
+# Locations of all diagnosis pattern-related files
+PATTERN_DIR = DATA_DIR / 'keywords/patterns'
+SYNONYM_DIR = DATA_DIR / 'keywords/synonyms'
+# Directory where all processed data is written to (cleaned text, embeddings, etc.)
+PROCESSED_DIR = DATA_DIR / 'processed'
+# Directory for saving and loading models
+MODEL_DIR = DATA_DIR / 'models'
+# Directory where evaluation metric-related files are saved to
+METRIC_DIR = DATA_DIR / 'evaluation_metrics'
 
 
-def parse_preprocessing_args():
+def create_parser():
 	'''
-	This function creates argument parser and parses a script's input arguments.
-	This is the most common way to define input arguments in python. It is used
-	by preprocess.py.
-
-	To change the parameters, pass them to the script, for example:
-
-	python cli/preprocess.py \
-		--dataset_dir output_dir \
-		--embedding_model average_word_embeddings_glove.840B.300d
-	
-	Default arguments have the meaning of being a reasonable default value, not of the last arguments used.
+	This function creates a default, general argument parser to act as a 
+	parent for scripts in this project that are run from the CLI.
+	This function is used in preprocess.py, train.py, and evaluate.py.
+	Default arguments have the meaning of being a reasonable default value.
 	'''
-	parser = argparse.ArgumentParser(description='Pre-process a network/graph of reddit comment data')
 
+	# General-purpose parser to use for different scripts
+	# Not intended to be used as a standalone parser in this project
+	parser = argparse.ArgumentParser(
+		description='General-use parser for CLI scripts in this project',
+		add_help=False # Don't add help, only because this is a parent parser
+	)
+
+	# File path arguments used throughout CLI scripts
 	parser.add_argument(
 		'--dataset_dir',
 		type=Path,
@@ -49,122 +57,25 @@ def parse_preprocessing_args():
 		help='Path to the directory where files containing their respective synonyms are stored.',
 	)
 	parser.add_argument(
-		'--match_dir',
-		type=Path,
-		default=MATCH_DIR,
-		help='Path to the directory where files containing their respective diagnosis matches are stored.',
-	)
-	parser.add_argument(
 		'--processed_dir',
 		type=Path,
 		default=PROCESSED_DIR,
-		help='Path to the directory where the pre-processed (i.e. cleaned) data is stored.',
+		help='Path to the directory where the pre-processed (i.e. cleaned) data is saved.',
 	)
 	parser.add_argument(
 		'--model_dir',
 		type=Path,
 		default=MODEL_DIR,
-		help='Path to the directory where trained models are saved/stored.',
+		help='Path to the directory where trained models are located.',
 	)
 	parser.add_argument(
-		'--evaluation_results_dir',
+		'--metric_dir',
 		type=Path,
-		default=EVALUATION_RESULTS_DIR,
-		help='Path to the directory where model evaluation files (i.e. plots/figures) are stored.',
-	)
-	# See www.sbert.net/docs/pretrained_models.html for pre-trained examples
-	parser.add_argument(
-		'--embedding_model',
-		type=str,
-		default='average_word_embeddings_glove.6B.300d',
-		help='The (word) embedding model to use for feature generation.'
-	)
-	parser.add_argument(
-		'--disorder_names',
-		'--list',
-		nargs='+',
-		type=lambda s: [str(disorder) for disorder in s.split(',')],
-		default=AVAILABLE_DISORDERS,
-		help='List of comma separated disorders to consider during preprocessing.'
+		default=METRIC_DIR,
+		help='Path to the directory where evaluation metric files are saved.'
 	)
 
-	args = parser.parse_args()
-	return args
-
-
-def parse_train_args():
-	'''
-	This function creates argument parser and parses a script's input arguments.
-	This is the most common way to define input arguments in python. It is used
-	by train.py.
-
-	To change the parameters, pass them to the script, for example:
-
-	python cli/train.py \
-		--dataset_dir output_dir \
-		--learning_rate 2e-3
-	
-	Default arguments have the meaning of being a reasonable default value, not of the last arguments used.
-	'''
-	parser = argparse.ArgumentParser(description='Train a model on a network/graph of reddit comment data')
-
-	# Required arguments
-	parser.add_argument(
-		'--dataset_dir',
-		type=Path,
-		default=DATASET_DIR,
-		help='Path to the directory where the dataset files are stored.',
-	)
-	parser.add_argument(
-		'--processed_dir',
-		type=Path,
-		default=PROCESSED_DIR,
-		help='Path to the directory where the pre-processed (i.e. cleaned) data is stored.',
-	)
-	parser.add_argument(
-		'--model_dir',
-		type=Path,
-		default=MODEL_DIR,
-		help='Path to the directory where trained models are saved/stored.',
-	)
-	parser.add_argument(
-		'--train_val_size',
-		type=int,
-		default=None,
-		help='Combined size (# samples) of the training and validation set.',
-	)
-	parser.add_argument(
-		'--test_size',
-		type=int,
-		default=None,
-		help='Size (# samples) of the test set.',
-	)
-	parser.add_argument(
-		'--percent_train',
-		type=float,
-		default=0.8,
-		help='Percentage of the data to use for training (train_val_size * percent_train).',
-	)
-	parser.add_argument(
-		'--shuffle_generator',
-		type=bool,
-		default=True,
-		help='Whether to shuffle training data.',
-	)
-	parser.add_argument(
-		'--shuffle_train_data',
-		type=bool,
-		default=True,
-		help='Whether to shuffle training data.',
-	)
-	parser.add_argument(
-		'--debug',
-		default=False,
-		action='store_true',
-		help='Whether to use a small subset of the dataset for debugging.',
-	)
-
-	# Dataset arguments
+	# Dataset arguments (i.e. network file names and their column header names)
 	parser.add_argument(
 		'--node_id_col',
 		type=str,
@@ -175,7 +86,7 @@ def parse_train_args():
 		'--edge_id_col',
 		type=str,
 		default='users',
-		help='Column header corresponding to the network edge id (i.e. what groups/relates src and tgt).',
+		help='Column header corresponding to the network edge id (i.e. what relates the src and tgt).',
 	)
 	parser.add_argument(
 		'--label_col',
@@ -207,6 +118,92 @@ def parse_train_args():
 		default='target_nodes',
 		help='Column header corresponding to the target node column.',
 	)
+	
+	# Return parser to use as a parent, so we DON'T want to call parse_args()
+	return parser
+
+
+def parse_preprocessing_args(parents=[]):
+	'''
+	This function creates a preprocessing-related argument parser and parses a
+	(preprocessing) script's input arguments.
+
+	Default arguments have the meaning of being a reasonable default value.
+	To change the parameters, pass them to the script. For example, assuming:
+	we have a parent parser with a dataset_dir argument:
+
+	python3 preprocess.py --dataset_dir=dataset --embedding_model=average_word_embeddings_glove.840B.300d
+	'''
+
+	# Create parser based on the parent parser(s)
+	# An empty list ([]) is equivalent to no parents
+	parser = argparse.ArgumentParser(
+		parents=parents,
+		description='Pre-process a network/graph of reddit comment data'
+	)
+
+	# Data preprocessing-related parameters
+	# See www.sbert.net/docs/pretrained_models.html for pre-trained examples
+	parser.add_argument(
+		'--embedding_model',
+		type=str,
+		default='average_word_embeddings_glove.6B.300d',
+		help='The (word) embedding model to use for feature generation.'
+	)
+
+	args = parser.parse_args()
+	return args
+
+
+def parse_train_args(parents=[]):
+	'''
+	This function creates a training-related argument parser and parses a
+	(training) script's input arguments.
+
+	Default arguments have the meaning of being a reasonable default value.
+	To change the parameters, pass them to the script. For example, assuming:
+	we have a parent parser with a processed_dir argument:
+
+	python3 train.py --processed_dir=dataset --learning_rate=2e-3
+	'''
+
+	# Create parser based on the parent parser(s)
+	# An empty list ([]) is equivalent to no parents
+	parser = argparse.ArgumentParser(
+		parents=parents,
+		description='Train a model on a network/graph of reddit comment data'
+	)
+
+	parser.add_argument(
+		'--train_val_size',
+		type=int,
+		default=None,
+		help='Combined size (# samples) of the training and validation set.',
+	)
+	parser.add_argument(
+		'--test_size',
+		type=int,
+		default=None, # None implies a separate test set will be used
+		help='Size (# samples) of the test set.',
+	)
+	parser.add_argument(
+		'--percent_train',
+		type=float,
+		default=0.8,
+		help='Percentage of the data to use for training (train_val_size * percent_train).',
+	)
+	parser.add_argument(
+		'--shuffle_generator',
+		type=bool,
+		default=True,
+		help='Whether to shuffle training data.',
+	)
+	parser.add_argument(
+		'--shuffle_train_data',
+		type=bool,
+		default=True,
+		help='Whether to shuffle training data.',
+	)
 
 	# Training arguments
 	parser.add_argument(
@@ -220,7 +217,7 @@ def parse_train_args():
 		'--model_location',
 		type=str,
 		default='trained_model',
-		help='The name of the file to save/load the model to/from.'
+		help='The model name to use for saving and loading a model.'
 	)
 	parser.add_argument(
 		'--batch_size',
@@ -303,10 +300,68 @@ def parse_train_args():
 	parser.add_argument(
 		'--layer_num_samples',
 		'--list',
-		nargs='+',
 		type=lambda s: [int(num_samples) for num_samples in s.split(',')],
 		default=[30, 10],
 		help='List of comma separated number of samples to use at each layer.'
+	)
+
+	args = parser.parse_args()
+	return args
+
+
+def parse_eval_args(parents=[]):
+	'''
+	This function creates a model evaluation-related argument parser and
+	parses a (model evaluation) script's input arguments.
+
+	Default arguments have the meaning of being a reasonable default value.
+	To change the parameters, pass them to the script. For example, assuming:
+	we have a parent parser with a metric_dir argument:
+
+	python3 evaluate.py --metric_dir=evaluation_metrics --metrics=scores,confusion_matrix
+	'''
+
+	# Create parser based on the parent parser(s)
+	# An empty list ([]) is equivalent to no parents
+	parser = argparse.ArgumentParser(
+		parents=parents,
+		description='Generate a evaluation metrics using model predictions.'
+	)
+
+	# Evaluation-related parameters
+	parser.add_argument(
+		'--metrics',
+		'--list',
+		type=lambda m: [str(metric) for metric in m.split(',')],
+		default=['scores', 'confusion_matrix', 'embedding_visualization'],
+		help='List of metrics/figures to generate. Defaults to all metrics.',
+	)
+
+	parser.add_argument(
+		'--pred_col',
+		type=str,
+		default='predicted',
+		help='Column header to use for predicted label IDs in test_predictions.csv.',
+	)
+
+	parser.add_argument(
+		'--true_col',
+		type=str,
+		default='actual',
+		help='Column header to use for true label IDs in test_predictions.csv.',
+	)
+
+	parser.add_argument(
+		'--save_prefix',
+		type=str,
+		default='',
+		help='Prefix to add to each metric file that is saved.',
+	)
+	parser.add_argument(
+		'--title_prefix',
+		type=str,
+		default='',
+		help='Prefix to use for figure titles (for metrics that use a title).'
 	)
 
 	args = parser.parse_args()
