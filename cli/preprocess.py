047d5a0923bf0a0d6e51ee240223b4ee52a1e69f
Chris Lewis <51756603+lewisc4@users.noreply.github.com>
2023-03-23 19:56:26 -0400

    Updated preprocess.py
    Updated preprocess.py to use the new FileManager and Preprocessor class changes. Also added filename constants for all files that are used for reading/writing.

--- cli/preprocess.py
+++ cli/preprocess.py
@@ -1,14 +1,31 @@
-from mhd_classifier.utils.cli_utils import parse_preprocessing_args, parse_train_args
+from mhd_classifier.utils.cli_utils import create_parser, parse_preprocessing_args
 from mhd_classifier.utils.file_utils import FileManager
 from mhd_classifier.utils.preprocessing_utils import Preprocessor
 from mhd_classifier.modeling.reddit_network import RedditNetwork
 from mhd_classifier.modeling.diagnosis_helper import Diagnoser
 
 
+# File name constants, files required BEFORE preprocessing
+NETWORK_F = 'network.csv'
+EDGE_LIST_F = 'edge_list.csv'
+DIAGNOSIS_PATTERN_F = 'diagnosis_patterns.txt'
+COMMON_PREFIX_F = 'common_prefixes.csv'
+SYNONYMS_DIR = 'synonyms'
+
+# File name constants, files written AFTER preprocessing
+CLEANED_NETWORK_F = 'cleaned_network.csv'
+DIAGNOSED_USERS_F = 'diagnosed_users.txt'
+EMBEDDINGS_F = 'embeddings.npy'
+
+# Mental health disorder names (i.e. labels) used in this project
+DISORDER_NAMES = ['anxiety', 'depression', 'adhd', 'ocd', 'eating_disorders']
+
+
 def main():
 	# Parse script args
-	args = parse_preprocessing_args()
-	args_train = parse_train_args()
+	script_parser = create_parser()
+	args = parse_preprocessing_args(parents=[script_parser])
+
 	# File handler to use for reading and writing preprocessed data
 	f_manager = FileManager()
 	# The preprocesser helper class we will use
@@ -20,47 +37,54 @@ def main():
 	f_manager.read(args.pattern_dir)
 	f_manager.read(args.synonym_dir)
 
-	# Get necessary files
-	network = f_manager.get('network.csv')
-	edge_list = f_manager.get('edge_list.csv')
-	diagnosis_patterns = f_manager.get('diagnosis_patterns.txt')
-	common_prefixes = f_manager.get('common_prefixes.txt')
-	synonyms = f_manager.get_dir('synonyms')
+	# Update the root directory to use for file I/O
+	f_manager.root = args.processed_dir
 
-	# Build network and clean data
+	# Get necessary files that we read from the directories above
+	network = f_manager.get(NETWORK_F)
+	edge_list = f_manager.get(EDGE_LIST_F)
+	diagnosis_patterns = f_manager.get(DIAGNOSIS_PATTERN_F)
+	common_prefixes = f_manager.get(COMMON_PREFIX_F)
+	synonyms = f_manager.get_dir(SYNONYMS_DIR)
+
+	# Build our Reddit network using the original data
 	reddit_network = RedditNetwork(
-		args=args_train,
+		args=args,
 		nodes=network,
 		edges=edge_list
 	)
-	cleaned = preprocessor.clean(df=reddit_network.nodes, field='text')
-	f_manager.write(args.processed_dir, 'cleaned_network.csv', cleaned)
+	# Clean each node's text (i.e. Reddit comment) and save the cleaned data
+	cleaned = preprocessor.clean(df=reddit_network.nodes)
+	f_manager.write(CLEANED_NETWORK_F, cleaned)
 
-	# Use cleaned data to get diagnosed users
+	# Create a Diagnoser to identify Reddit comments containing self-diagnoses
 	diagnoser = Diagnoser(
-		disorder_list=args.disorder_names,
+		disorder_list=DISORDER_NAMES,
 		patterns=diagnosis_patterns,
 		synonym_map=synonyms,
 		common_prefixes=common_prefixes
 	)
+	# Search the cleaned Reddit comments for self-reported diagnoses
 	diagnosed_users = diagnoser.get_diagnoses(cleaned)
-	f_manager.write(args.processed_dir, 'diagnosed_users.txt', diagnosed_users)
+	f_manager.write(DIAGNOSED_USERS_F, diagnosed_users)
 
-	# Get custom stopwords and remove them from cleaned text
-	# After removing stopwords, embed the text to use as features
-	disorder_syns = [synonyms[d] for d in args.disorder_names]
+	# Get the synonyms for each disorder
+	disorder_syns = [synonyms[d] for d in DISORDER_NAMES]
 	all_synonyms = [syn for syn_list in disorder_syns for syn in syn_list]
+
+	# Remove stopwords from the cleaned data to generate feature embeddings
+	# We consider the disorder synonyms to be stopwords and also remove them,
+	# because we don't want to "cheat" by using them in our feature embeddings
 	no_stopwords = preprocessor.remove_stopwords(
 		df=cleaned,
-		field='text',
 		custom_stopwords=all_synonyms
 	)
+	# Get embeddings using the cleaned data (with no stopwords) and save them
 	embeddings = preprocessor.embed(
-		df=no_stopwords,
-		field='text',
-		embedding_type=args.embedding_model
+		embedding_type=args.embedding_model,
+		df=no_stopwords
 	)
-	f_manager.write(args.processed_dir, 'embeddings.npy', embeddings)
+	f_manager.write(EMBEDDINGS_F, embeddings)
 
 
 if __name__ == '__main__':
