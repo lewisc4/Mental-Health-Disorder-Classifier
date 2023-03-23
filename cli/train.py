3e60888c12c7298f50039f84e20caffaca881a88
Chris Lewis <51756603+lewisc4@users.noreply.github.com>
2023-03-23 19:58:29 -0400

    Updated train.py
    Updated train.py to now pass the dataset as a dictionary, with train/val/test entries, as opposed to separate train/val/test objects. Also updated to include the changes to the FileManager class and added filename constants.

--- cli/train.py
+++ cli/train.py
@@ -1,54 +1,47 @@
-from mhd_classifier.utils.cli_utils import parse_train_args
+from mhd_classifier.utils.cli_utils import create_parser, parse_train_args
 from mhd_classifier.utils.file_utils import FileManager
 from mhd_classifier.utils.preprocessing_utils import Preprocessor
 from mhd_classifier.modeling.reddit_network import RedditNetwork
 from mhd_classifier.modeling.ml_models import GraphSAGEModel, GCNModel, GATModel
 
 
-def instantiate_model(graph, train_data, val_data, test_data, args):
-	if args.model_type == 'GraphSAGE':
-		return GraphSAGEModel(
-			graph=graph,
-			train_data=train_data,
-			val_data=val_data,
-			test_data=test_data,
-			args=args
-		)
-	elif args.model_type == 'GCN':
-		return GCNModel(
-			graph=graph,
-			train_data=train_data,
-			val_data=val_data,
-			test_data=test_data,
-			args=args
-		)
-	else:
-		return GATModel(
-			graph=graph,
-			train_data=train_data,
-			val_data=val_data,
-			test_data=test_data,
-			args=args
-		)
+# File name constants, files required BEFORE training
+CLEANED_NETWORK_F = 'cleaned_network.csv'
+EDGE_LIST_F = 'edge_list.csv'
+EMBEDDINGS_F = 'embeddings.npy'
+DIAGNOSED_USERS_F = 'diagnosed_users.txt'
+
+# File name constants, files written AFTER training
+TEST_EMBEDDINGS_F = 'test_embeddings.npy'
+TEST_PREDICTIONS_F = 'test_predictions.csv'
+
+# Maps model_type key to its model class
+MODEL_TYPE_MAP = {
+	'GraphSAGE': GraphSAGEModel,
+	'GCN': GCNModel,
+	'GAT': GATModel,
+}
 
 
 def main():
 	# Parse script args
-	args = parse_train_args()
-	# File handler to use for reading and writing data
-	f_manager = FileManager()
-	# The preprocesser helper class we will use
-	preprocessor = Preprocessor()
+	script_parser = create_parser()
+	args = parse_train_args(parents=[script_parser])
 
+	# File handler to use for reading and writing data
 	f_manager = FileManager()
 	f_manager.read(args.dataset_dir)
 	f_manager.read(args.processed_dir)
 
-	nodes = f_manager.get('cleaned_network.csv')
-	edges = f_manager.get('edge_list.csv')
-	embeddings = f_manager.get('embeddings.npy')
-	diagnosed_users = f_manager.get('diagnosed_users.txt')
+	# Update the root directory to use for file I/O
+	f_manager.root = args.processed_dir
 
+	# Get necessary data to construct our network
+	nodes = f_manager.get(CLEANED_NETWORK_F)
+	edges = f_manager.get(EDGE_LIST_F)
+	embeddings = f_manager.get(EMBEDDINGS_F)
+	diagnosed_users = f_manager.get(DIAGNOSED_USERS_F)
+	# Build our Reddit network
 	reddit_network = RedditNetwork(
 		args=args,
 		nodes=nodes,
@@ -57,28 +50,21 @@ def main():
 		ground_truth=diagnosed_users
 	)
 
+	# Get StellarGraph graph and to a dataset representation of our network
 	graph = reddit_network.to_stellargraph()
 	dataset = reddit_network.to_dataset()
+	# Example on getting class weights from on dataset (not actually used)
 	class_weights = reddit_network.get_class_weights(dataset)
 
-	args.learning_rate = 0.02
-	args.num_train_epochs = 60
-	args.batch_size = 300
-	args.layer_size = 32
-	args.model_type = 'GCN'
+	# Instantiate a graph model using the provided model type and arguments
+	model = MODEL_TYPE_MAP.get(args.model_type, GCNModel)(graph, dataset, args)
+	# Train our model and save it
+	trained_model = model.train()
+	trained_model.save(args.model_dir / args.model_location)
 
-	model = instantiate_model(
-		graph=graph,
-		train_data=dataset['train'],
-		val_data=dataset['val'],
-		test_data=dataset['test'],
-		args=args
-	)
-	model.train(save=True, class_weight=class_weights)
-
-	test_embeddings, predictions_df = model.test()
-	f_manager.write(args.processed_dir, 'test_embeddings.npy', test_embeddings)
-	f_manager.write(args.processed_dir, 'test_predictions.csv', predictions_df)
+	test_embeddings, predictions_df = model.test(model_to_use=trained_model)
+	f_manager.write(TEST_EMBEDDINGS_F, test_embeddings)
+	f_manager.write(TEST_PREDICTIONS_F, predictions_df)
 
 
 if __name__ == '__main__':
